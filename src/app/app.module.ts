cb804de2f33ebf28043b8eb4fb1a34acc8a96e2d
csaba.adam <csaba.adam@shiwaforce.com>
2018-09-28 08:37:53 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	// az összes modul amit ismernie kell
+	// az index.html-nek induláshoz
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
