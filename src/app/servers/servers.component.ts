b45f5f77fd79a0b98f434fa813bb66f052a58064
csaba.adam <csaba.adam@shiwaforce.com>
2018-09-28 11:21:42 +0200

    asd

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	//selector: '[app-servers]',	// attribute
+	//selector: '.app-servers',	// class
+	selector: 'app-servers',	// tag
+	/* template: `
+  		<app-server></app-server>
+		  <app-server></app-server>`, */
+	templateUrl: './servers.component.html',
+	styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+	allowNewServer = false;
+	serverCreationStatus = 'No server ceated yet';
+	serverName = 'TestServer';
+
+	constructor() {
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000)
+	}
+
+	ngOnInit() {
+	}
+
+	// best practice "on" prefix
+	onCreateServer() {
+		this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+	}
+
+	onUpdateServerName(event: any) {
+		// console.log(event);
+		this.serverName = (<HTMLInputElement>event.target).value;
+	}
+
+}
